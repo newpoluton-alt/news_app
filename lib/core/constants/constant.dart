9e911b1379a7392409f158a38a50a29f2dd5ac73
Aziret <newpoluton@gmail.com>
2024-11-10 18:21:25 +0100

    Remove sensitive file from tracking

--- lib/core/constants/constant.dart
+++
@@ -1,4 +0,0 @@
-const String newsApiKey = "7be10787d5d1463291db52f8f13c2aa4";
-const String newsApiBaseUrl = "https://newsapi.org/v2";
-const String newsApiCountry = "us";
-const String newsApiCategory = "business";
